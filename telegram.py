4a4bf3f35a16a0ae0a5598c2cb0d9d702d9a6e3d
Алексей Сухов <alexskyman@icloud.com>
2020-09-18 10:18:29 +0300

    fj

--- telegram.py
+++ telegram.py
@@ -5,41 +5,27 @@ import json
 
 app = Flask(__name__)
 
-
-
-# def RequestItilium(dict_data):
-#     print("thread:" + GetCurrentThread() + "stack: RequestItilium")
-#     try:
-#        quote = "\""
-#        response = requests.post(address_api_itilium, data=json.dumps(dict_data).encode('utf-8'),
-#                             auth=(login_itilium, password_itilium))
-#        code = response.status_code
-#        description = response.text
-#        print("thread:" + GetCurrentThread() + "  code: " + str(code))
-#        print("thread:" + GetCurrentThread() + "  description: " + description)
-#        if (code == 200):
-#            return False, description, True
-#        else:
-#            return False, str(code) + ' ' + description, False
-#     except:
-#        return True, "Ошибка соединения с Итилиум. Обратитесь к администратору.", False
+address_api_itilium = os.environ['AddressApiItilium']
+login_itilium = os.environ['LoginItilium']
+password_itilium = os.environ['PasswordItilium']
+auth_key =  os.environ['AuthKey'] 
 
 
 @app.route('/getWebhookInfo',  methods=['GET'])
 def getWebHookInfo():
-    ret = requests.post("https://api.telegram.org/bot" + os.environ['AUTH_KEY_BOT'] + "/getWebhookInfo")
+    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/getWebhookInfo")
     return ret.text
 
 @app.route('/setWebHook',  methods=['GET'])
 def setWebHook():
     dict_data = dict()
-    address = request.url.replace("setWebHook",os.environ['AUTH_KEY_BOT'])
+    address = request.url.replace("setWebHook",auth_key)
     print(address)
     dict_data.update( {"url": address} )
     
     print(dict_data)
     
-    ret = requests.post("https://api.telegram.org/bot" + os.environ['AUTH_KEY_BOT'] + "/setWebhook", 
+    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook", 
         data = json.dumps(dict_data).encode('utf-8'), 
         headers = {"Content-Type" : "application/json"})
     
@@ -52,18 +38,14 @@ def removeWebHook():
     dict_data = dict()
     dict_data.update( {"url": ""} )
     
-    ret = requests.post("https://api.telegram.org/bot" + os.environ['AUTH_KEY_BOT'] + "/setWebhook", 
+    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook", 
         data = json.dumps(dict_data).encode('utf-8'), 
         headers = {"Content-Type" : "application/json"})
     
     return ret.text
 
-# @app.route('/' + os.environ['AUTH_KEY_BOT'],  methods=['GET'])
-# def test():
-#     print("rrrrrr")
-#     return "sdsd"
 
-@app.route('/' + os.environ['AUTH_KEY_BOT'],  methods=['POST'])
+@app.route('/' + auth_key,  methods=['POST'])
 def IncomingConnectionPost():
     print("new message")
     return Response(status=200)
